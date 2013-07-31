dafccd941ab29a8c83eb538a6830961465dfbc23
santi <santiagoangel10@gmail.com>
2013-07-31 18:51:11 +0200

    search for artist and label with soundcloud account

--- scrapers/views.py
+++ scrapers/views.py
@@ -12,8 +12,12 @@ from django.template import RequestContext
 import simplejson
 
 from django.contrib.auth.decorators import login_required
-
 from scrapers.models import *
+
+
+sc_client_id = 'a0b4638bae6d50a9296f7fc3f35442eb'
+
+
 
 #@login_required(login_url='/accounts/login/')
 def home(request):
@@ -24,9 +28,9 @@ def home(request):
 		artist_names = []
 		for artist in artists:
 			artist_names.append(artist['name'])
-		query = get_stream_query(artist_names)
+		query = get_stream_query('artist', artist_names)
 
-		releases = get_browse_releases('stream', query)
+		releases, rgids = get_browse_releases('stream', query)
 
 
 		#return HttpResponse("Username found: %s" % username )
@@ -57,24 +61,38 @@ def get_lastfm_artists(username, number):
 			artists.append({"name":artist["name"], "mbid":artist['mbid']})
 		return artists
 
-def get_stream_query(artists):
+def get_stream_query(query_type, mbids):
+	# query_type = 'artist'
 	today = datetime.date.today()
-	last_month = today - datetime.timedelta(days=60)
+	last_month = today - datetime.timedelta(days=30)
 	end_date = today.strftime('%Y-%m-%d')
 	start_date = last_month.strftime('%Y-%m-%d')
 
-	query = 'date:['+start_date +" TO "+ end_date+']'
-	query += "AND("
-	for artist in artists:
-		if artists.index(artist) == 0:
-			query = query + 'artist: "' + artist + '"'
+	if query_type == 'artist' or 'label':
+		query = "("
+	else:
+		query = 'date:['+start_date +" TO "+ end_date+']'
+		query += " AND "
+		query += "("
+
+	#if query_type == 'laid':
+		# for mbid in mbids:
+		# 	if mbids.index(mbid) == 0:
+		# 		query = query + query_type+':' + mbid + ' '
+		# 	else:
+		# 		query = query + ' OR '+query_type+':' + mbid + ' '
+	#else:
+	for mbid in mbids:
+		if mbids.index(mbid) == 0:
+			query = query + query_type+': "' + mbid + '" '
 		else:
-			query = query + ' OR artist:"' + artist + '"'
+			query = query + 'OR '+query_type+':"' + mbid + '" '
 
 	query += ')'
-	query = urllib.quote_plus(query.encode('utf8'))
 	print query
+	query = urllib.quote_plus(query.encode('utf8'))
 	return query 
+
 
 #@login_required(login_url='/accounts/login/')
 def get_album_tracks(request):
@@ -102,7 +120,7 @@ def get_album_tracks(request):
 
 
 def artist_browse(request, artist_id):
-	releases = get_browse_releases('artist', artist_id)
+	releases, rgids = get_browse_releases('artist', artist_id)
 	return render_to_response('scrapers/home.html',{'page':'artist', 'releases':releases}, context_instance=RequestContext(request))
 	#return HttpResponse("artist id: " +artist_id)
 
@@ -110,25 +128,26 @@ def artist_browse(request, artist_id):
 
 
 def label(request, label_id):
-	releases = get_browse_releases('label', label_id)
+	releases, rgids = get_browse_releases('label', label_id)
 	#return render_to_response('scrapers/home.html',{'page':'label', 'releases':releases})
 	#return HttpResponse("label id: " +label_id)
 	return render_to_response('scrapers/home.html',{'page':'label', 'releases':releases}, context_instance=RequestContext(request))
 
 
 
-def get_browse_releases(query_type, mbid, rgids = None):
+def get_browse_releases(query_type, mbid_or_query, rgids = None):
 	offset = 0
 	releases = []
 	if rgids == None:
 		rgids = []
 	while True:
 		if query_type == 'label':
-			url = "http://www.musicbrainz.org/ws/2/release?label="+mbid+"&fmt=json&limit=100&offset="+str(offset)+"&inc=artist-credits+release-groups+labels"
+			url = "http://www.musicbrainz.org/ws/2/release?label="+mbid_or_query+"&fmt=json&limit=100&offset="+str(offset)+"&inc=artist-credits+release-groups+labels"
 		if query_type == 'artist':
-			url = "http://www.musicbrainz.org/ws/2/release?artist="+mbid+"&fmt=json&limit=100&offset="+str(offset)+"&inc=artist-credits+release-groups+labels"
+			url = "http://www.musicbrainz.org/ws/2/release?artist="+mbid_or_query+"&fmt=json&limit=100&offset="+str(offset)+"&inc=artist-credits+release-groups+labels"
 		if query_type == 'stream':
-			url = "http://www.musicbrainz.org/ws/2/release?&query="+mbid+ "&limit=100&fmt=json&offset="+str(offset)
+			url = "http://www.musicbrainz.org/ws/2/release?&query="+mbid_or_query+ "&limit=100&fmt=json&offset="+str(offset)
+		print "THIS IS THE QUERY URL: \n" + url
 		data = urllib2.urlopen(url)
 		api_results = json.load(data)
 		if api_results["releases"] == []:
@@ -138,7 +157,7 @@ def get_browse_releases(query_type, mbid, rgids = None):
 		rgids += parsed_results['rgids']
 		offset +=100
 	#return render_to_response('scrapers/home.html',{'page':query_type, 'releases':releases})
-	return releases	
+	return releases, rgids
 
 
 def parse_releases(api_results, rgids):
@@ -212,30 +231,86 @@ def lastfm_search(request):
 		#return HttpResponse("No username")
 		return render_to_response('scrapers/home.html', {'page':'lastfm_search'}, context_instance=RequestContext(request))
 
+
+def soundcloud_search(request):
+	get = request.GET.copy()
+	username = get['username']
+	query_type = get['query-type']
+	print "GET REQUEST username: " + username
+	print "get request type: " +query_type
+	#query_type = get['query-type']
+	limit = 200
+	offset = 0
+	names = []
+	while True:
+		url = 'http://api.soundcloud.com/users/'+username+'/followings.json?client_id='+sc_client_id+'&limit='+str(limit)+'&offset='+str(offset)
+		print "Soundcloud API url: " + url
+		data = urllib2.urlopen(url)
+		api_results = json.load(data)
+		for entry in api_results:
+			name = entry['username']
+			names.append(name)
+		if len(api_results) < limit:
+			break
+		else:
+			offset += limit
+
+	# query_type = 'artist'
+	query = get_stream_query(query_type, names)
+	api_results = mbz_search(query, query_type)
+	print "results search sees: "
+	print api_results
+	results = []
+	if query_type == 'label':
+		# query_type = 'labels'
+		for entry in api_results['labels']:
+			print "api_results length: " + str(len(api_results))
+			# print "ENTRY: "
+			# print entry 
+			results.append({'name':entry['name'], 'mbid':entry['id']})
+		return render_to_response('scrapers/home.html',{'page':'soundcloud', 'labels':results},context_instance=RequestContext(request))
+	elif query_type == 'artist':
+		for entry in api_results['artist']:
+			results.append({'name':entry['name'], 'mbid':entry['id']})
+		return render_to_response('scrapers/home.html',{'page':'soundcloud', 'artists':results},context_instance=RequestContext(request))
+
+
 def search(request):
 	if request.GET:
 		get = request.GET.copy()
 		query = get['query']
 		query = urllib.quote_plus(query.encode('utf8'))
+		query_type = get['query-type']
 		print "query: " +query
 		#query_type = get['query-type']
-		query_type = 'artist'
-		results = mbz_search(query, query_type)
-		artists = []
-		for artist in results['artist']:
-			artists.append({'name':artist['name'], 'mbid':artist['id']})
-		#return HttpResponse("Username found: %s" % username )
-		return render_to_response('scrapers/home.html',{'page':'search', 'artists':artists},context_instance=RequestContext(request))
-		# call username API
-		# send to template that charts out each 
+		#query_type = 'artist'
+		api_results = mbz_search(query, query_type)
+		print "results search sees: "
+		print api_results
+		results = []
+		if query_type == 'label':
+			# query_type = 'labels'
+			for entry in api_results['labels']:
+				print "api_results length: " + str(len(api_results))
+				# print "ENTRY: "
+				# print entry 
+				results.append({'name':entry['name'], 'mbid':entry['id']})
+			return render_to_response('scrapers/home.html',{'page':'search', 'labels':results},context_instance=RequestContext(request))
+		elif query_type == 'artist':
+			for entry in api_results['artist']:
+				results.append({'name':entry['name'], 'mbid':entry['id']})
+			return render_to_response('scrapers/home.html',{'page':'search', 'artists':results},context_instance=RequestContext(request))
+
 	else:
 		#return HttpResponse("No username")
 		return render_to_response('scrapers/home.html', {'page':'search'}, context_instance=RequestContext(request))
 
-def mbz_search(query, type):
-	url = "http://musicbrainz.org//ws/2/"+type+"/?query="+query+"&fmt=json"
+def mbz_search(query, query_type, limit =100):
+	url = "http://musicbrainz.org/ws/2/"+query_type+"/?query="+query+"&fmt=json&limit="+str(limit)
+	print "QUERY URL: " + url
 	data = urllib2.urlopen(url)
 	api_results = json.load(data)
+	#print api_results
 	return api_results
 
 
@@ -248,8 +323,8 @@ def follow_toggle(request):
 
 		if post['type'] == 'artist':
 			print "type: artist "
-			artist_name = post['artist_name']
-			print artist_name 
+			# artist_name = post['artist_name']
+			# print artist_name 
 			artist_id = post['artist_id']
 			print artist_id
 			# artist_name = get_english_alias(artist_id)
@@ -311,34 +386,34 @@ def get_english_alias(lookup_type, mbid):
 		name = results['name']
 	return name
 
-# def stream(request):
-# 	if request.user.is_authenticated():
-# 		artist_set = request.user.get_profile().artists.all()
-# 		print artist_set
-# 		artist_ids = []
-# 		for artist in artist_set:
-# 			artist_ids.append(artist.mbid)
+def stream(request):
+	if request.user.is_authenticated():
+		artist_set = request.user.get_profile().artists.all()
+		print artist_set
+		artist_ids = []
+		for artist in artist_set:
+			artist_ids.append(artist.mbid)
 
-# 		label_set = request.user.get_profile().artists.all()
-# 		label_ids = []
-# 		for label in label_set:
-# 			label_ids.append(label.mbid)
+		label_set = request.user.get_profile().labels.all()
+		label_ids = []
+		for label in label_set:
+			label_ids.append(label.mbid)
 
-# 		rgids = []
-# 		releases = []
+		rgids = []
+		releases = []
 
-# 		query = get_stream_query('arid', artist_ids)
-# 		new_releases, new_rgids = get_browse_releases('stream', query, rgids)
-# 		releases +=new_releases
-# 		rgids+= new_rgids
+		query = get_stream_query('arid', artist_ids)
+		new_releases, new_rgids = get_browse_releases('stream', query, rgids)
+		releases +=new_releases
+		rgids+= new_rgids
 
-# 		query = get_stream_query('laid', label_ids)
-# 		new_releases, new_rgids = get_browse_releases('stream', query, rgids)
-# 		releases +=new_releases
-# 		rgids+= new_rgids
+		query = get_stream_query('laid', label_ids)
+		new_releases, new_rgids = get_browse_releases('stream', query, rgids)
+		releases +=new_releases
+		rgids+= new_rgids
 
-# 		return render_to_response('scrapers/home.html',{'page':'stream', 'releases':releases}, context_instance=RequestContext(request))
+		return render_to_response('scrapers/home.html',{'page':'stream', 'releases':releases}, context_instance=RequestContext(request))
 
-# 	else:
-# 		HttpResponse("You need to be logged in to have a stream")
+	else:
+		return HttpResponse("You need to be logged in to have a stream")
 
